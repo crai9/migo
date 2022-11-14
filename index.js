f17c70028c95ac31c131efe6d8a25784c35f4d25
Craig Miller <8229723+crai9@users.noreply.github.com>
2022-11-14 18:25:54 +0000

    Slight refactoring

--- index.js
+++ index.js
@@ -2,57 +2,47 @@ let cron = require('cron');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-const token = 'NDc5NDk3NTA5MTExMzMyODc0.GEB2Xc.s262dZYt8AQpB5v6EqBTKeflddaLHjhiscsDbs';
-const test_server_id = '479484990837489665';
-const server_id = '473632099354673152';
-const miguel_id = '152190878247682048';
+const botToken = process.env.BOT_TOKEN;
+const serverId = '473632099354673152';
+const miguelId = '152190878247682048';
 
 const date = new Date('2022-12-09T07:00:00Z');
 const emojis = '🎢 🇫🇷 --> '
 
+if (typeof botToken == 'undefined' || botToken === null) {
+    console.error('Bot token not set');
+    process.exit(9);
+}
+
 client.on('ready', () => {
-    console.log('ready!');
+    console.log('Ready!');
 
     let updateTitle = cron.job("*/1 * * * *", function(){
-
         let title = emojis + time_till_date(date);
-        console.info('ran cron and got title -> ' + title);
-        client.guilds.find(val => val.id === server_id).setName(title);
-
+        client.guilds.find(val => val.id === serverId).setName(title).then(r => console.info('Set server name to: ' + r.name));
     });
 
     updateTitle.start();
-
 });
 
 client.on('message', message => {
-
     if (message.content === '!days')
     {
-        message.reply(emojis + time_till_date(date));
+        message.reply(emojis + time_till_date(date))
+            .catch(console.error);
     }
 
-    let react = true;
-    if (react === true)
-    {
-        if(message.content.includes('mig') || message.author.id === miguel_id)
-        {
-            if(message.guild)
-            {
-                message.react(message.guild.emojis.find(val => val.name === 'migo'))
-                    .catch(console.error);
-            }
+    if (message.content.includes('mig') || message.author.id === miguelId) {
+        if (message.guild) {
+            message.react(message.guild.emojis.find(val => val.name === 'migo'))
+                .catch(console.error);
         }
     }
-
-
 });
 
 client.on('error', console.error);
 
 function time_till_date(date2) {
-
     let ONE_HOUR = 1000 * 60 * 60;
     let date1 = new Date();
 
@@ -66,7 +56,8 @@ function time_till_date(date2) {
     let hours = Math.floor(total_hours % 24);
 
     return Math.floor(days) + 'Days, ' + hours + 'Hours';
-
 }
 
-client.login(token);
+client.login(botToken)
+    .then(token => console.info('Logged in with token: ' + token))
+    .catch(token => console.error('Issue logging in with token: ' + token));
